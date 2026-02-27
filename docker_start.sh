ea80fff248199d349e6127172282754cc508052e
Simon-Olivier Lachance <monstrotac19@hotmail.com>
2026-02-27 11:54:30 -0500

    Update docker_start.sh

--- docker_start.sh
+++ docker_start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ ! -f /config/config.js ]; then
-  cp /usr/src/csgofloat/config.js /config/config.js
-  echo "Copied config to /config/config.js. Please edit this file and restart the container."
+if [ ! -f /config/bots.json ]; then
+  echo '[{"user":"bot1","pass":"password","auth":"shared_secret"}]' > /config/bots.json
+  echo "Created template /config/bots.json. Please edit this file with your bot credentials and restart the container."
   exit 1
 fi
 
 mkdir -p /config/steam_data
 
-exec node index.js -c /config/config.js -s /config/steam_data
+exec node index.js -s /config/steam_data
